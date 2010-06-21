ab3ba0f0d8ea90ed26455bb6663e0f970210aac8
szumny <szumny@cb4ffc43-3023-c6db-d580-6e8a05bdc34c>
2010-06-21 23:01:09 +0000

    beta version 0.9.5 + binary
    changelog:
    + Pacman vs. Monsters - implemented
    + Pacman vs. Ghosts - implemented
    + MagicPillsGameObject - added, implemented
    + improvements in searchingPacman algorithm
    + improvements in loading move graphic for Pacman/Monsters/Ghosts
    + correct manu title position
    - disabling two maps

---
+++ Pacman/Pacman/Pacman/GameObjects/MoveableGameObject.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Pacman.GameManager;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pacman.GameObjects
+{
+    abstract class MoveableGameObject : GameObject
+    {
+        #region Enums
+
+        public enum direction
+        {
+            Right = 0,
+            Down = 1,
+            Left = 2,
+            Up = 3
+        }
+
+        #endregion
+        
+        #region Fields
+
+        protected direction nextMove;
+        protected direction movementDirection;
+
+        protected const int FRAME_COUNT = 3;
+        protected const int TOLERANCE = 2;
+        protected const float STEP = 3.0f;
+
+        protected int updateDelta;
+
+        #endregion
+
+        #region Properties
+
+        public Rectangle ScreenRectanglePosition
+        {
+            get { return new Rectangle((int)screenVectorPosition.X, (int)screenVectorPosition.Y, GameObject.OBJECT_SIZE * GameObject.SCALE, GameObject.OBJECT_SIZE * GameObject.SCALE); }
+            set { screenVectorPosition.X = value.X; screenVectorPosition.Y = value.Y; }
+        }
+
+        public Vector2 ScreenVectorPosition
+        {
+            get { return screenVectorPosition; }
+        }
+
+        public GameCoordinates GameVectorPosition
+        {
+            get {
+                return (GameCoordinates.ScreenPositionToGameCoortinates(
+                    new Vector2(ScreenVectorPosition.X+TOLERANCE, screenVectorPosition.Y+TOLERANCE)));
+            }
+        }
+
+        #endregion
+
+        #region Update
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+
+            //TODO: remove unnecessary field updateDelta here and in MonstarGameObject
+            updateDelta += gameTime.ElapsedGameTime.Milliseconds;
+
+
+            bool moved = makeMove();
+
+
+
+            updateDelta = 0;
+
+        }
+
+        public abstract bool makeMove();
+        
+
+        protected bool WasOnVerticalPath()
+        {
+            return movementDirection == direction.Down || movementDirection == direction.Up;
+        }
+
+        protected bool WasOnHorizontalPath()
+        {// <------------->
+            return movementDirection == direction.Right || movementDirection == direction.Left;
+        }
+
+        protected Vector2 CalculateMove(direction direction, int updateDelta, float STEP)
+        {
+            Vector2 move = Vector2.Zero;
+            switch (direction)
+            {
+                case direction.Up:
+                    move = new Vector2(0.0f, -STEP * 24 * ((float)updateDelta) / 1000);
+                    break;
+
+                case direction.Down:
+                    move = new Vector2(0.0f, STEP * 24 * ((float)updateDelta) / 1000);
+                    break;
+
+                case direction.Left:
+                    move = new Vector2(-STEP * 24 * (((float)updateDelta) / 1000), 0.0f);
+                    break;
+
+                case direction.Right:
+                    move = new Vector2(STEP * 24 * (((float)updateDelta) / 1000), 0.0f);
+                    break;
+            }
+
+            return move;
+        }
+
+        protected Rectangle roundRectangleToPath(Rectangle screenRectangle, Vector2 move, direction direction, bool onHorizontalPath, bool onVerticalPath)
+        {
+            screenRectangle.X += (int)move.X;
+            screenRectangle.Y += (int)move.Y;
+
+            if (onVerticalPath && (direction != direction.Right && direction != direction.Left)) //if it on pathX, then should be exactly on this path
+            {
+                //round to neerest 24*X
+                float roundX = (((int)screenRectangle.X) % GameObject.OBJECT_SIZE) / (float)GameObject.OBJECT_SIZE;
+                /*base*/
+                screenRectangle.X = (((int)screenRectangle.X) / GameObject.OBJECT_SIZE) * GameObject.OBJECT_SIZE;
+                screenRectangle.X += (int)(Math.Round(roundX) * OBJECT_SIZE);
+            }
+
+            if (onHorizontalPath && (direction != direction.Up && direction != direction.Down)) //if it on pathY, then should be exactly on this path
+            {
+                //round to neerest 24*Y
+                float roundY = (((int)screenRectangle.Y) % GameObject.OBJECT_SIZE) / (float)GameObject.OBJECT_SIZE;
+                /*base*/
+                screenRectangle.Y = (((int)screenRectangle.Y) / GameObject.OBJECT_SIZE) * GameObject.OBJECT_SIZE;
+                screenRectangle.Y += (int)(Math.Round(roundY) * OBJECT_SIZE);
+            }
+
+
+            return screenRectangle;
+
+        }
+
+        #endregion 
+
+        #region HandleInput and Draw
+
+        public abstract override void HandleInput(KeyboardState keyboardState);
+
+        public abstract override void Draw(GameTime gameTime);
+
+        #endregion
+
+    }
+}
---
+++ Pacman/Pacman/Pacman/GameObjects/ToolBarGameObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace Pacman.GameObjects
+{
+    class ToolBarGameObject : GameObject
+    {
+
+        int delta, lastUpdate;
+        int points;
+        bool levelCompleted;
+
+        public ToolBarGameObject()
+        {
+            points = 0;
+        }
+
+        public override void LoadContent()
+        {
+
+            lastUpdate = DotGameObject.VisibleDotCounter-1;
+        }
+
+        public override void Update(Microsoft.Xna.Framework.GameTime gameTime, bool otherScreenHasFocus, bool coveredByOtherScreen)
+        {
+
+            delta = lastUpdate - DotGameObject.VisibleDotCounter;
+
+            points += delta*10;
+
+            lastUpdate = DotGameObject.VisibleDotCounter;
+
+            if (delta>0)
+            { 
+                //System.Console.WriteLine("VisibleDotCounter: " + DotGameObject.VisibleDotCounter.ToString());
+            }
+
+            if (DotGameObject.VisibleDotCounter == 0)
+            { 
+                //levelCompleted = true;
+                
+            }
+        }
+
+        public override void Draw(Microsoft.Xna.Framework.GameTime gameTime)
+        {
+            GameObject.SpriteBatch.DrawString(gameFontCourierNew20, "Points: " + points.ToString(), new Vector2(24, 19 * 24), Color.White);
+
+            if (levelCompleted)
+            {
+                String msg = "Level completed!";
+                Vector2 vec = gameFontCourierNew40.MeasureString(msg);
+                Color color;
+                if (gameTime.TotalGameTime.Milliseconds  < 500)
+                    color = Color.White;
+                else
+                    color = Color.LightGreen;
+
+                GameObject.SpriteBatch.DrawString(gameFontCourierNew40, msg, new Vector2(640 / 2 - vec.X / 2, 480 / 2 - vec.Y / 2), color);
+            }
+        }
+    }
+}
